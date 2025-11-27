3c1f0bf7878a25a4fbdbdc821d02f9e82345a078
Dmc <dimoygt@gmail.com>
2025-11-27 11:07:27 -0400

    v1.0.9

--- prisma.config.js
+++ prisma.config.js
@@ -1,25 +1,27 @@
-const defineConfig = require("prisma/config").defineConfig;
-const env = require("prisma/config").env;
-module.exports = defineConfig({
-  schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
-  engine: "classic",
-  datasource: {
-    url: process.env.DATABASE_URL,
-  }
-});
-
-// import { defineConfig, env } from "prisma/config";
-
-// export default defineConfig({
+// const dotenv = require("dotenv")
+// dotenv.config();
+// const defineConfig = require("prisma/config").defineConfig;
+// const env = require("prisma/config").env;
+// module.exports = defineConfig({
 //   schema: "prisma/schema.prisma",
 //   migrations: {
 //     path: "prisma/migrations",
 //   },
 //   engine: "classic",
 //   datasource: {
-//     url: env("DATABASE_URL"),
-//   },
+//     url: process.env.DATABASE_URL,
+//   }
 // });
+
+import { defineConfig, env } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  engine: "classic",
+  datasource: {
+    url: env("DATABASE_URL"),
+  },
+});
